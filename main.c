736dac6d3dbb6f6a5ff567c10523a193e77c68ef
dx-dt <deexdete@gmail.com>
2014-11-09 00:35:33 +0100

    added function to list solved problems

--- main.c
+++ main.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <limits.h>
+#include <string.h>
 #include "problem_list.h"
+
+int number_of_problems = sizeof(solved_problems)/sizeof(*solved_problems); 
 
 /* prints proper usage of program and exits returning non-zero value */
 void fail(char **argv){
-	printf("usage: %s <problem number>\n",argv[0]);
+	printf("usage: %s [-l|--list] <problem number>\n\n  -l, --list\tlist solved problems, with link to problem on projecteuler.net\n",argv[0]);
 	exit(EXIT_FAILURE);
 }
 
@@ -17,11 +20,25 @@ int intfromstr (char str[]){
 	return (int)long_integer;
 }
 
-/* checks if arguments are what we expect or fails */
+/* pretty selfexplanatory, isn't it? */
+void list_solved_problems(void){
+	int i;
+	printf("\nThe following problems are solved:\n\n");
+	for(i = 0; i <= number_of_problems-1; i++){
+		int n = solved_problems[i].problem_id;
+		printf("Problem number: %i\thttp://projecteuler.net/problem=%i\n",n,n);
+	}
+	exit(EXIT_SUCCESS);
+}
+
+/* checks if arguments are what we expect and redirects or fails */
 void check_args(int argc, char **argv){
 	if(argc != 2){
 		fail(argv);
 	}	
+	if(strcmp(argv[1],"-l") == 0 || strcmp(argv[1],"--list") == 0){
+		list_solved_problems();
+	}
 	int problem_number = intfromstr(argv[1]);
 	if(problem_number <= 0){
 		fail(argv);
@@ -29,10 +46,9 @@ void check_args(int argc, char **argv){
 	return;
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv){	
 	check_args(argc,argv);
 	int problem_number = intfromstr(argv[1]);
-	int number_of_problems = sizeof(solved_problems)/sizeof(*solved_problems); 
 	int i;
 	/* iterate over list of solved problems, and if the input matches a solved problem calls it's corresponding function pointer, see problem_list.h for list of solved problems */
 	for(i = 0; i <= number_of_problems-1; i++){
